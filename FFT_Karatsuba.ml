ef83d06a3d0adfcde6c2d164d90c810545e68d48
Covah <kheireddine.ounnoughi@etu.sorbonne-universite.fr>
2024-12-08 23:32:57 +0100

    remove unnecessary stuff

--- FFT_Karatsuba.ml
+++ FFT_Karatsuba.ml
@@ -92,14 +92,6 @@ type expr =
   | Add of expr list
   | Mul of expr list;; 
 
-(*QS 1.6*)
-let expr_tree =
-  Add [
-    Mul [Int 123; Pow [Var "x";Int 1]];
-    Int 42;
-    Pow [Var "x";Int 3]
-  ];;
-      
 (* QS 1.7*)
 let rec arb2poly expr_tree =
   match expr_tree with 
@@ -143,10 +135,6 @@ let extraction_alea list_L list_P =
     let elt = List.nth list_L r in 
     (remove_element r list_L, elt::list_P);;
 
-let list1 = [1;2;3];;
-let list2 = [5;6;7];;
-extraction_alea list1 list2;;
-  
 (* QS 1.9 *)
 let gen_permutation n =
   let rec generate_list upperBound count = match count with
@@ -158,7 +146,6 @@ let gen_permutation n =
     else fill_list (extraction_alea l p)
   in fill_list (generate_list n 1,[]);;
 
-gen_permutation 10;; 
 
 (* QS 1.10 *)
 
@@ -179,8 +166,6 @@ let abr list =
     | h::t -> buildABR t (insertABR h tree)
   in buildABR list Empty;;
 
-let list_permutation = gen_permutation 10;;
-let abr_test = abr list_permutation;;
 
 (* QS 1.11 *) 
 
@@ -202,7 +187,6 @@ let rec etiquetage tree =
     else 
       Mul [etiquetage left; etiquetage right];; 
   
-let etiq_test = etiquetage abr_test;; 
 
 (* QS 1.12 *)
 
@@ -233,18 +217,6 @@ let rec gen_arb tree =
 
 
 
-let val_test9 =
-  Add
-    [Var "x";
-     Add
-       [Mul
-          [Add [Int 34; Mul [Mul [Int (-5); Var "x"]; Mul [Int 80; Var "x"]]];
-           Int 21];
-        Mul [Var "x"; Add [Int 44; Pow [Var "x"; Int 69]]]]];;
-
-let gen_arb_test = gen_arb val_test9;; 
-
-let test9 = gen_arb etiq_test;;
       
 (* Expérimentations *)                              
 (* QS 2.13 *)
@@ -257,56 +229,6 @@ let rec transform_ABR l =
   | [] -> []
   | [h] -> [gen_arb (etiquetage h)]
   | h::t -> gen_arb (etiquetage h) :: transform_ABR t;;
-
-(*let list_ABR = generer_n_ABR 200 20;;
-let transformed_ABR_list = transform_ABR list_ABR;;*)
-
-(* QS 2.14 *)
-
-(* Naive strategy : convert each tree into a polynomial and then add them together *)
-let add_naive l =
-  let rec list2polyList treeList =
-    match treeList with
-    | [] -> []
-    | h::t -> (arb2poly h) :: (list2polyList t)
-  in 
-  let rec sumPolyList treeList =
-    match treeList with
-    | [] -> []
-    | [h] -> h
-    | h::t -> poly_add h (sumPolyList t)
-  in sumPolyList (list2polyList l);;
-
-(* Fusion strategy: create a new tree with a new Add node at the root that is linked to each tree 
-   and convert this new tree to a polynomial *)
-let add_fusion l = arb2poly (Add l);;
-
-(* Divide and conquer strategy: split the problem into smaller subproblems, 
-   solve them, and combine the results *)
-let add_divide l = 
-  let rec divide_and_conquer trees =
-    let rec split lst =
-      let rec aux l1 l2 count = 
-        match l2 with
-        | [] -> (List.rev l1, [])
-        | h :: t -> 
-            if count = 0 then (List.rev l1, l2)
-            else aux (h :: l1) t (count - 1)
-      in 
-      aux [] lst (List.length lst / 2)
-    in 
-    match trees with
-    | [] -> []
-    | [h] -> arb2poly h
-    | _ -> let (left, right) = split trees in poly_add (divide_and_conquer left) (divide_and_conquer right)
-  in divide_and_conquer l;;
-  
-let list_ABR2 = generer_n_ABR 100 20;;
-let transformed_ABR_list2 = transform_ABR list_ABR2;; 
-
-let testNaiveExo14 = add_naive transformed_ABR_list2;;
-let testFusionExo14 = add_fusion transformed_ABR_list2;; 
-let testDivideExo14 = add_divide transformed_ABR_list2;;
 
 (* Evaluate the execution time of a function *) 
 let time f x =
@@ -328,77 +250,7 @@ let printTime f x =
 let rec generate_trees n = 
   if n = 1000 then [transform_ABR (generer_n_ABR n 20)]
   else (transform_ABR (generer_n_ABR n 20)) :: generate_trees (n+100);; 
-                                                      
-(* Execute the tests with n = 100, ..., 1000 and store the execution time *)
-let rec addNaiveTest l = match l with
-  | [] -> [] 
-  | h::t -> time add_naive h :: addNaiveTest t;;
 
-let rec addFusionTest l = match l with
-  | [] -> [] 
-  | h::t -> time add_fusion h :: addFusionTest t;;
-
-let rec addDivideTest l = match l with
-  | [] -> [] 
-  | h::t -> time add_divide h :: addDivideTest t;;
-
-(*
-let treesQ14Q15 = generate_trees 100;;
-let addNaiveDurationQ14 = addNaiveTest treesQ14Q15;;
-let addFusionDurationQ14 = addFusionTest treesQ14Q15;;
-let addDivideDurationQ14 = addDivideTest treesQ14Q15;;
-*)
-(* QS 2.15 *)
-
-(* Naive strategy : convert each tree into a polynomial and then multiply them together *)
-let prod_naive l =
-let rec list2polyList treeList =
-match treeList with
-| [] -> []
-| h::t -> (arb2poly h) :: (list2polyList t)
-in
-let rec prodPolyList treeList =
-match treeList with
-| [] -> []
-| [h] -> h
-| h::t -> poly_prod h (prodPolyList t)
-in prodPolyList (list2polyList l);;
-
-(* Fusion strategy : same as the addition but with a Mul node *)
-let prod_fusion l = arb2poly (Mul l);;
-
-(* Divide and conquer strategy : same as the addition but with poly_prod *)
-let prod_divide l =
-  let rec divide_and_conquer lst left right =
-    if right - left <= 1 then
-      arb2poly (List.nth lst left)
-    else
-      let mid = (left + right) / 2 in
-      poly_prod 
-        (divide_and_conquer lst left mid)
-        (divide_and_conquer lst mid right)
-  in
-  divide_and_conquer l 0 (List.length l)
-;;
-
-(* Execute the tests with n = 100, ..., 1000 and store the execution time *)
-let rec prodNaiveTest l = match l with
-  | [] -> [] 
-  | h::t -> time prod_naive h :: prodNaiveTest t;;
-
-let rec prodFusionTest l = match l with
-  | [] -> [] 
-  | h::t -> time prod_fusion h :: prodFusionTest t;;
-
-let rec prodDivideTest l = match l with
-  | [] -> [] 
-  | h::t -> time prod_divide h :: prodDivideTest t;;
-
-  (*
-let prodNaiveDurationQ15 = prodNaiveTest treesQ14Q15;;
-let prodFusionDurationQ15 = prodFusionTest treesQ14Q15;;
-let prodDivideDurationQ15 = prodDivideTest treesQ14Q15;;
-*)
 (* Same tests *)
 (* Additionne deux polynomes poly1 et poly2 *)
 
@@ -814,5 +666,4 @@ let rec arb2poly_karatsuba_2 expr_tree =
   let timeFFT = printTime list2polyList_FFT arb1 ;;
   Printf.printf("Karatsuba v2 tree gen : \n");;
   let timekaratsuba = printTime list2polyList_karatsuba2 arb1;;
-
 
